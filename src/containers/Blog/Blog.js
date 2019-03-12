6e4998e3ccaff41c448f789726f6ed7cbd56962f
Jefersson Bustamante <leonardofunez@Leonardos-iMac.local>
2019-03-12 16:04:54 -0500

    Final http react tutorial

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+
 class Blog extends Component {
+    state = { 
+        posts: [],
+        selectedPostID: null
+    }
+    componentDidMount(){
+        axios.get('/posts')
+        .then(response => {
+            const posts = response.data.slice(0, 4)
+            const updatePosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'MAX'
+                }
+            });
+            this.setState({posts: updatePosts});
+        }).catch(error => {
+            console.log(error)
+        });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostID: id})
+    }
+
     render () {
+        const post = this.state.posts.map(post => {
+            return <Post 
+                key={post.id} 
+                title={post.title}
+                author={post.author} 
+                clicked={() => this.postSelectedHandler(post.id)}/>;
+        }); 
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {post}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostID}/>
                 </section>
                 <section>
                     <NewPost />
